5ae4583e3e27868ac11cd6e6bf1f53880aea3eee
Calvin Chen <yc2929@nyu.edu>
2017-10-09 23:11:53 -0400

    Assignment_10/05
    Team Member: Ammanuel Daniel

--- sketch.js
+++ sketch.js
@@ -1,17 +1,56 @@
-//A series of 3 rectangles in a row, each rotated to a different angle.
-//The same series of rectangles, made with a for loop controlling their rotation angle.
 
+// Partner is Calvin Chen
+var r;
 function setup(){
-	createCanvas(1000,500);
-	background(255);
+
+	createCanvas(900,900);
+
 }
 
 function draw(){
-	var rot = map(mouseY,0,1000,0,PI);
-    rotate(rot);
-	fill(0);
-	noStroke();
-    rect(100,200,100,100);
-    rect(300,200,100,100);
-    rect(500,200,100,100);
+	background(255, 207, 104);
+// rotation
+var mapRot = map(mouseX, 0, width, 0, 2*PI);
+//Shape within Matrix 
+push();
+
+translate(mouseX,mouseY);
+
+if (mouseIsPressed) {
+	rotate(mapRot)
+	createStar(0, 0, 7);
 }
+
+pop();
+
+}
+//click mouse and star stamp
+	function createStar(x, y, points){
+
+var mapColor = map(mouseX,0, width, 150, 255);
+var mapColor1 = map(mouseX,0, width, 100, 150);
+var mapColor2 = map(mouseX,0, width, 170, 255);
+var mapColor0 = map(mouseX,0, width,0, 255);
+
+	r = ((2*PI)/points);
+//star
+	fill(0,0,mapColor0);
+		beginShape();
+		for(var i = 0; i < (2*PI) ; i += r){
+			var a = x + cos(i)*15;
+			var b = y + sin(i)*15;
+			var c = x + cos(i+(r/2))*30;
+			var d = y + sin(i+(r/2))*30;
+			vertex(a,b);
+			vertex(c,d);
+
+		}
+		endShape(CLOSE);
+		// Circle
+	fill(mapColor, mapColor1, mapColor2);
+
+		ellipse(0,0,30,30);
+		
+
+	}
+
